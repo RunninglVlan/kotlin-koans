514ac4a399309c52f3092ff6bbb3123ac9705e7f
vladimirs.muravskis <Vladimirs.Muravskis@twino.eu>
2018-05-24 18:11:59 +0300

    Refactored/Reformatted i_introduction and iii_conventions   Fixed compilation warnings

--- test/i_introduction/_7_Nullable_Types/N07NullableTypesKtTest.kt
+++ test/i_introduction/_7_Nullable_Types/N07NullableTypesKtTest.kt
@@ -4,46 +4,51 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class N07NullableTypesKtTest {
-    fun testSendMessageToClient(
-            client: Client?,
-            message: String?,
-            email: String? = null,
-            shouldBeInvoked: Boolean = false
-    ) {
-        var invoked = false
-        sendMessageToClient(client, message, object : Mailer {
-            override fun sendMessage(actualEmail: String, actualMessage: String) {
-                invoked = true
-                assertEquals("The message is not as expected:",
-                        message, actualMessage)
-                assertEquals("The email is not as expected:",
-                        email, actualEmail)
-            }
-        })
-        assertEquals("The function 'sendMessage' should${if (shouldBeInvoked) "" else "n't"} be invoked",
-                shouldBeInvoked, invoked)
-    }
+	fun testSendMessageToClient(
+			client: Client?,
+			expectedMessage: String?,
+			expectedEmail: String? = null,
+			shouldBeInvoked: Boolean = false
+	) {
+		var invoked = false
+		sendMessageToClient(client, expectedMessage, object : Mailer {
+			override fun sendMessage(email: String, message: String) {
+				invoked = true
+				assertEquals("The message is not as expected:",
+						expectedMessage, message)
+				assertEquals("The email is not as expected:",
+						expectedEmail, email)
+			}
+		})
+		assertEquals("The function 'sendMessage' should${if (shouldBeInvoked) "" else "n't"} be invoked",
+				shouldBeInvoked, invoked)
+	}
 
-    @Test fun everythingIsOk() {
-        testSendMessageToClient(Client(PersonalInfo("bob@gmail.com")),
-                "Hi Bob! We have an awesome proposition for you...",
-                "bob@gmail.com",
-                true)
-    }
+	@Test
+	fun everythingIsOk() {
+		testSendMessageToClient(Client(PersonalInfo("bob@gmail.com")),
+				"Hi Bob! We have an awesome proposition for you...",
+				"bob@gmail.com",
+				true)
+	}
 
-    @Test fun noMessage() {
-        testSendMessageToClient(Client(PersonalInfo("bob@gmail.com")), null)
-    }
+	@Test
+	fun noMessage() {
+		testSendMessageToClient(Client(PersonalInfo("bob@gmail.com")), null)
+	}
 
-    @Test fun noEmail() {
-        testSendMessageToClient(Client(PersonalInfo(null)), "Hi Bob! We have an awesome proposition for you...")
-    }
+	@Test
+	fun noEmail() {
+		testSendMessageToClient(Client(PersonalInfo(null)), "Hi Bob! We have an awesome proposition for you...")
+	}
 
-    @Test fun noPersonalInfo() {
-        testSendMessageToClient(Client(null), "Hi Bob! We have an awesome proposition for you...")
-    }
+	@Test
+	fun noPersonalInfo() {
+		testSendMessageToClient(Client(null), "Hi Bob! We have an awesome proposition for you...")
+	}
 
-    @Test fun noClient() {
-        testSendMessageToClient(null, "Hi Bob! We have an awesome proposition for you...")
-    }
+	@Test
+	fun noClient() {
+		testSendMessageToClient(null, "Hi Bob! We have an awesome proposition for you...")
+	}
 }
--- test/iii_conventions/N25ComparisonKtTest.kt
+++ test/iii_conventions/N25ComparisonKtTest.kt
@@ -5,23 +5,23 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class N25ComparisonKtTest {
-    /* Month numbering starts with 0 (0-Jan, 1-Feb, ... 11-Dec) */
-    @Test fun testDateComparison() {
-        assertTrue(task25(MyDate(2014, 1, 1), MyDate(2014, 1, 2)))
-    }
+	/* Month numbering starts with 0 (0-Jan, 1-Feb, ... 11-Dec) */
+	@Test
+	fun testDateComparison() {
+		assertTrue(task25(MyDate(2014, 1, 1), MyDate(2014, 1, 2)))
+	}
 
-    @Test fun testBefore() {
-        val first = MyDate(2014, 5, 10)
-        val second = MyDate(2014, 7, 11)
-        assertTrue("The date ${first.s} should be before ${second.s}", first < second)
-    }
+	@Test
+	fun testBefore() {
+		val first = MyDate(2014, 5, 10)
+		val second = MyDate(2014, 7, 11)
+		assertTrue("The date ${first.s} should be before ${second.s}", first < second)
+	}
 
-    @Test fun testAfter() {
-        val first = MyDate(2014, 10, 20)
-        val second = MyDate(2014, 7, 11)
-        assertTrue("The date ${first.s} should be after ${second.s}", first > second)
-    }
-
-    /* If you declare 'compareTo' as an extension function, remove this one to make the code compile */
-    operator fun MyDate.compareTo(other: MyDate): Int = todoTask25()
+	@Test
+	fun testAfter() {
+		val first = MyDate(2014, 10, 20)
+		val second = MyDate(2014, 7, 11)
+		assertTrue("The date ${first.s} should be after ${second.s}", first > second)
+	}
 }
